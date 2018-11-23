81e7172ba6651b02d3af1862a73ecc529301cc3a
Santhosh <csesanthosh15@gmail.com>
2018-11-23 18:58:28 +0530

    add content to about section

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,15 +1,36 @@
 <template>
-    <div>
-        <h2>About</h2>
-    </div>
+  <div id="about">
+    <section class="about">
+      <h2 class="heading">About Me</h2>
+      <p class="position">Developer</p>
+    </section>
+    <section class="content">
+        <p>I am a Computer Science Graduate. I build websites and mobile applications.
+            <br>
+Loves stalking computer and mobile screens in search of cool stuff. <br>I am authour <a href="http://bleedbytes.in?ref=portfolio" target="_blank">Bleedbytes</a>, a tech blog.</p>
+    </section>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
+.heading {
+  font-size: 15vmin;
+  color: #0D1B2A;
+}
 
+.position {
+    color: blueviolet;
+    font-weight: bold;
+    font-size: 5vmin;
+}
+
+.content {
+  padding: 5vmin;
+  font-size: 5vmin;
+  color: #0D1B2A;
+}
 </style>
