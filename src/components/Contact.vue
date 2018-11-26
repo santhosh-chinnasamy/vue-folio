bf17535f98d5af3952b6dba1f3be2d362aa0938b
Santhosh <csesanthosh15@gmail.com>
2018-11-26 16:32:45 +0530

    add links to socialnetworks

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,15 +1,78 @@
 <template>
+  <div>
+    <h2 class="heading">Contact</h2>
     <div>
-        <h2>Contact</h2>
+      <ul class="links">
+        <li class="social" v-for="item in social" :key="item.id">
+          <a :href="item.url" target="_blank">{{item.title}}</a>
+        </li>
+      </ul>
+
+
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      break1: "<br/>",
+      social: [
+        {
+          id: 1,
+          title: "LinkedIn",
+          url: "https://www.linkedin.com/in/santhosh-chinnasamy/",
+          icon: "fab fa-twitter"
+        },
+        {
+          id: 2,
+          title: "Github",
+          url: "https://github.com/santhosh-chinnasamy"
+        },
+        {
+          id: 3,
+          title: "Twitter",
+          url: "https://twitter.com/santhosh_in_dev"
+        },
+        {
+          id: 4,
+          title: "Blog",
+          url: "https://bleedbytes.in"
+        },
+        {
+          id: 5,
+          title: "Email",
+          url: "mailto:csesanthosh15@gmail.com?subject=from portfolio"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style scoped>
+.heading {
+  font-size: 15vmin;
+  font-family: "Finger Paint", cursive;
+  font-weight: lighter;
+  color: #0d1b2a;
+}
+/* .links{
+    width: 70%;
+} */
+a {
+  color: #42b883;
+}
 
+.social {
+  font-weight: bolder;
+  display: inline;
+  padding: 10px;
+  font-size: 13vmin;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+li {
+  list-style: none;
+}
 </style>
